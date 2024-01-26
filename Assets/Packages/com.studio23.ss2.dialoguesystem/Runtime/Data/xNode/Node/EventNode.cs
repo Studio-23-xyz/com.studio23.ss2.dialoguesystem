621a2429fa425b63b13b07de4d07472ed85da826
B-DeshiDev <ffalsoy@gmail.com>
2024-01-26 11:46:41 +0600

    Rename event

--- Assets/Packages/com.studio23.ss2.dialoguesystem/Runtime/Data/xNode/Node/EventNode.cs
+++ Assets/Packages/com.studio23.ss2.dialoguesystem/Runtime/Data/xNode/Node/EventNode.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 using XNode;
 
 namespace Studio23.SS2.DialogueSystem.Data
@@ -7,11 +8,11 @@ namespace Studio23.SS2.DialogueSystem.Data
     [NodeTint(200,50,50), NodeWidth(500), CreateNodeMenu("Unity Event Node")]
     public class EventNode : EventNodeBase
     {
-        [SerializeField] UnityEvent _event;
+        public UnityEvent Event;
 
         public override void Invoke()
         {
-            _event.Invoke();
+            Event.Invoke();
         }
 
     }
