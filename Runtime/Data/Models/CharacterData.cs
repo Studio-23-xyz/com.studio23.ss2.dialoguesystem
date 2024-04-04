e6262a786f62475cd71641d3512efb2f5c606410
B-DeshiDev <ffalsoy@gmail.com>
2024-04-04 09:06:24 +0600

    package ver update

---
+++ Runtime/Data/Models/CharacterData.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Studio23.SS2.DialogueSystem.Data
+{
+    [CreateAssetMenu(menuName = "Studio-23/Dialogue System/New Character Data")]
+    [Serializable]
+    public class CharacterData : ScriptableObject
+    {
+        public string CharacterName;
+        public Color DialogueColor;
+        public List<CharacterExpressionData> Expressions;
+
+        public CharacterExpressionData GetExpressionByName(string expressionName)
+        {
+            return Expressions.FirstOrDefault(e => e.ExpressionName == expressionName);
+        }
+        
+        public int GetExpressionIndexByName(CharacterExpressionData expressionData)
+        {
+            return Expressions.IndexOf(expressionData);
+        }
+    }
+}
---
+++ Runtime/Data/xNode/Node/DialogueStartNode.cs
@@ -0,0 +1,50 @@
+﻿using Cysharp.Threading.Tasks;
+using XNode;
+
+namespace Studio23.SS2.DialogueSystem.Data
+{
+    [NodeTint("#006600"), CreateNodeMenu("DialogueStartNode")]
+    public class DialogueStartNode : DialogueNodeBase
+    {
+        [Node.Output(typeConstraint = TypeConstraint.Strict, connectionType = ConnectionType.Override)]
+        public DialogueLineNodeBase Exit;
+        public override DialogueNodeBase GetNextNode()
+        {
+            NodePort outputPort = GetExitPort();
+            if (outputPort == null || outputPort.Connection == null)
+            {
+                return null;
+            }
+            return outputPort.Connection.node as DialogueNodeBase;
+        }
+        public override object GetValue(NodePort port)
+        {
+            if (port.fieldName == "Exit")
+            {
+                return this;
+                
+            }
+            return base.GetValue(port);
+        }
+
+        public override void HandleDialogueAdvance()
+        {
+            //do nothing
+        }
+
+        public override void HandleChoiceSelected(int choiceIndex)
+        {
+            //do nothing
+        }
+
+        public override  UniTask Play()
+        {
+            return UniTask.CompletedTask;
+        }
+        
+        public override void Initialize()
+        {
+            //do nothing
+        }
+    }
+}
