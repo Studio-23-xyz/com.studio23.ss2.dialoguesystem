11d5980e110e3a24689bcc7aad5ad556ad979dd8
B-DeshiDev <ffalsoy@gmail.com>
2024-04-30 10:20:23 +0600

    Update package.json

---
+++ Samples/DialogueChoiceDemo/DialogueStartHelper.cs
@@ -0,0 +1,23 @@
+using Studio23.SS2.DialogueSystem.Core;
+using Studio23.SS2.DialogueSystem.Data;
+using UnityEngine;
+
+namespace Samples
+{
+    public class DialogueStartHelper:MonoBehaviour
+    {
+        [SerializeField] private DialogueGraph _graph;
+        public DialogueGraph Graph => _graph;
+        public DialogueNodeBase StartNode;
+
+        public void StartDialogueFromNode()
+        {
+            DialogueSystem.Instance.StartDialogue(StartNode);
+        }
+        
+        public void StartDialogueFromDefaultStartNode()
+        {
+            DialogueSystem.Instance.StartDialogue(_graph);
+        }
+    }
+}
