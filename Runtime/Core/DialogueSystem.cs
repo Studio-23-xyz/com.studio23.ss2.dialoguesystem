51bffb3149bd762182913c5e6d100371c24778db
B-DeshiDev <ffalsoy@gmail.com>
2024-04-30 13:07:03 +0600

    Update package.json

---
+++ Runtime/Core/DialogueSystem.cs
@@ -0,0 +1,143 @@
+
+using System;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.DialogueSystem.Data;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Studio23.SS2.DialogueSystem.Core
+{
+    public class DialogueSystem : MonoBehaviour
+    {
+        public static DialogueSystem Instance;
+
+        [Header("Data")]
+        [SerializeField] private DialogueGraph _currentGraph;
+        [Header("Execution Data")]
+        [SerializeField] private DialogueNodeBase _curNode;
+
+        public event Action<DialogueGraph> OnDialogueStarted; 
+        public event Action<DialogueGraph> OnDialogueEnded; 
+        
+        public event Action<DialogueChoicesNode> OnDialogueChoiceStarted; 
+        public event Action<DialogueChoicesNode> OnDialogueChoiceEnded; 
+        public delegate void DialogueLineEvent(DialogueLineNodeBase dialogueLineNodeBase);
+        
+        /// <summary>
+        /// Subscribe to this to know when dialogue started
+        /// </summary>
+        public DialogueLineEvent DialogueLineStarted;
+
+        /// <summary>
+        /// Subscribe to this to know when dialogue completed
+        /// </summary>
+        public DialogueLineEvent DialogueLineCompleted;
+        
+
+        void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        /// <summary>
+        /// Initialize A new Dialogue Graph
+        /// </summary>
+        /// <param name="newGraph"></param>
+        public void InitializeDialogueGraph(DialogueGraph newGraph)
+        {
+            _currentGraph = newGraph;
+        }
+
+
+        /// <summary>
+        /// Start playing dialogue and it Fires the Start event
+        /// </summary>
+        public void StartDialogue(DialogueGraph graph)
+        {
+            PlayDialogue(graph).Forget();
+        }
+        
+        public void StartDialogue(DialogueNodeBase node)
+        {
+            PlayDialogue(node).Forget();
+        }
+        
+        public void StartDialogue()
+        {
+            PlayDialogue(_currentGraph).Forget();
+        }
+
+        public async UniTask PlayDialogue(DialogueGraph graph)
+        {
+            graph.Initialize();
+            await PlayDialogue(graph, graph.StartNode);
+        }
+        
+        public async UniTask PlayDialogue(DialogueNodeBase startNode)
+        {
+            var graph = startNode.graph as DialogueGraph;
+            graph.Initialize();
+            await PlayDialogue(graph, startNode);
+        }
+        
+        public async UniTask PlayDialogue(DialogueGraph graph, DialogueNodeBase startNode)
+        {
+            _currentGraph = graph;
+            _curNode = startNode;
+            _currentGraph.HandleDialogueStarted();
+            OnDialogueStarted?.Invoke(_currentGraph);
+            while (_curNode != null)
+            {
+                Debug.Log("Play = " + _curNode, _curNode);
+                await _curNode.Play();
+                
+                _curNode = _curNode.GetNextNode();
+            }
+
+            _currentGraph.HandleDialogueEnded();
+            OnDialogueEnded?.Invoke(_currentGraph);
+        }
+
+        public void AdvanceDialogue()
+        {
+            if (_curNode != null)
+            {
+                _curNode.HandleDialogueAdvance();
+            }
+        }
+
+        public void PickChoice(int choiceIndex)
+        {
+            if (_curNode != null)
+            {
+                _curNode.HandleChoiceSelected(choiceIndex);
+            }
+        }
+        
+        public void HandleDialogueChoiceStarted(DialogueChoicesNode dialogueChoicesNode)
+        {
+            OnDialogueChoiceStarted?.Invoke(dialogueChoicesNode);
+        }
+
+        public void HandleDialogueChoiceEnded(DialogueChoicesNode dialogueChoicesNode)
+        {
+            OnDialogueChoiceEnded?.Invoke(dialogueChoicesNode);
+        }
+
+        private void OnDestroy()
+        {
+            if (_currentGraph != null)
+            {
+                _currentGraph.Cleanup();
+            }
+        }
+    }
+}
+
---
+++ Runtime/Data/xNode/Node/DialogueChoicesNode.cs
@@ -0,0 +1,126 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.DialogueSystem.Utility;
+using UnityEngine;
+using UnityEngine.Serialization;
+using XNode;
+
+namespace Studio23.SS2.DialogueSystem.Data
+{
+    [NodeTint("#AAAA00"), CreateNodeMenu("Dialogue Choice Branch")]
+    public class DialogueChoicesNode : DialogueNodeBase
+    {
+        [Input]
+        public DialogueLineNodeBase Entry;
+        [Output]
+        public DialogueChoicesNode Choices;
+        
+        private int _lastChoiceIndex = -1;
+
+        protected List<DialogueChoiceNodeBase> _availableDialogueChoices;
+        public List<DialogueChoiceNodeBase> AvailableDialogueChoices => _availableDialogueChoices;
+
+        public override object GetValue(NodePort port)
+        {
+            if (port.fieldName == "Choices")
+            {
+                return this;
+            }
+            return base.GetValue(port);
+        }
+
+        protected virtual void PrepareDialogueChoices()
+        {
+            GetAvailableChoices();
+            SetChoiceIndices();
+        }
+
+        protected virtual void GetAvailableChoices()
+        {
+            FetchAllConnectedChoiceNodes();
+            RemoveUnavailableChoices();
+        }
+
+
+        public List<DialogueChoiceNodeBase> FetchAllConnectedChoiceNodes()
+        {
+            if (_availableDialogueChoices == null)
+            {
+                _availableDialogueChoices = new List<DialogueChoiceNodeBase>();
+            }
+            else
+            {
+                _availableDialogueChoices.Clear();
+            }
+
+            this.GetOutputNodesConnectedToPort("Choices", _availableDialogueChoices);
+            return _availableDialogueChoices;
+        }
+
+        private void SetChoiceIndices()
+        {
+            for (int i = 0; i < _availableDialogueChoices.Count; i++)
+            {
+                _availableDialogueChoices[i].DialogueChoiceIndex = i;
+            }   
+        }
+
+        protected void RemoveUnavailableChoices()
+        {
+            for (int i = _availableDialogueChoices.Count-1; i >= 0; i--)
+            {
+                var choice = _availableDialogueChoices[i];
+                if (!choice.CheckConditions())
+                {
+                    _availableDialogueChoices.RemoveAt(i);
+                }
+            }
+        }
+
+        public override void HandleDialogueAdvance()
+        {
+            //do nothing
+        }
+
+        public override DialogueNodeBase GetNextNode()
+        {
+            if (_lastChoiceIndex < 0 || _lastChoiceIndex >= _availableDialogueChoices.Count)
+            {
+                Debug.LogError($"INDEX {_lastChoiceIndex} OUT OF RANGE for dialogue choices {this}", this);
+            }
+            return _availableDialogueChoices[_lastChoiceIndex];
+        }
+
+        public override void HandleChoiceSelected(int choiceIndex)
+        {
+            //no value checks. 
+            //we assume that the index is valid
+            _lastChoiceIndex = choiceIndex;
+            if (_lastChoiceIndex >= 0 && _lastChoiceIndex < _availableDialogueChoices.Count)
+            {
+                var pickedChoice = _availableDialogueChoices[_lastChoiceIndex];
+                pickedChoice.HandleChoiceTaken();
+            }
+        }
+
+        public override async UniTask Play()
+        {
+            _lastChoiceIndex = -1;
+            PrepareDialogueChoices();
+            Core.DialogueSystem.Instance.HandleDialogueChoiceStarted(this);
+            
+            while (_lastChoiceIndex < 0)
+            {
+                await UniTask.Yield();
+            }
+            
+            Core.DialogueSystem.Instance.HandleDialogueChoiceEnded(this);
+        }
+
+        public override void Initialize()
+        {
+            //do nothing
+        }
+    }
+}
