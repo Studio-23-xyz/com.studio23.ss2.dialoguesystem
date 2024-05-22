f91e495e0034691d221f17fc6decb77e27669c93
B-DeshiDev <ffalsoy@gmail.com>
2024-05-22 12:20:47 +0600

    Update DialogueStartNode.cs

--- Assets/Packages/com.studio23.ss2.dialoguesystem/Runtime/Data/xNode/Node/DialogueStartNode.cs
+++ Assets/Packages/com.studio23.ss2.dialoguesystem/Runtime/Data/xNode/Node/DialogueStartNode.cs
@@ -1,4 +1,5 @@
 ﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 using XNode;
 
 namespace Studio23.SS2.DialogueSystem.Data
@@ -8,6 +9,9 @@ namespace Studio23.SS2.DialogueSystem.Data
     {
         [Node.Output(typeConstraint = TypeConstraint.Strict, connectionType = ConnectionType.Override)]
         public DialogueLineNodeBase Exit;
+        
+        [Output(typeConstraint =  TypeConstraint.InheritedInverse)] 
+        public EventNodeBase Events;
         public override DialogueNodeBase GetNextNode()
         {
             NodePort outputPort = GetExitPort();
@@ -39,12 +43,37 @@ namespace Studio23.SS2.DialogueSystem.Data
 
         public override  UniTask Play()
         {
+            InvokePostPlayEvents();
             return UniTask.CompletedTask;
         }
         
         public override void Initialize()
         {
             //do nothing
+        }
+        
+        public virtual void InvokePostPlayEvents()
+        {
+            var eventNodePort= GetOutputPort("Events");
+            for (int i = 0; i < eventNodePort.ConnectionCount; i++)
+            {
+                var eventNodeConnection = eventNodePort.GetConnection(i);
+                if (eventNodeConnection != null)
+                {
+                    if (eventNodeConnection.node is EventNodeBase eventNode)
+                    {
+                        eventNode.Invoke();
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"{this} Events port no {i} is connected to {eventNodeConnection.node} but it's not an event node", this);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"{this} Events port no {i} is connected to {eventNodeConnection} but connection null", this);
+                }
+            }
         }
     }
 }
