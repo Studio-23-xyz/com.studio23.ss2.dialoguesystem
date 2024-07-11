5946071bd54ffe1b971237d627e951c4bebd3257
B-DeshiDev <ffalsoy@gmail.com>
2024-07-11 10:44:08 +0600

    package ver bump

---
+++ Editor/Data/LineSpeakerDataPropertyDrawer.cs
@@ -0,0 +1,84 @@
+using System.Linq;
+using Studio23.SS2.DialogueSystem.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Data
+{
+    [CustomPropertyDrawer(typeof(LineSpeakerData))]
+    public class LineSpeakerDataPropertyDrawer:PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return base.GetPropertyHeight(property, label) * 3;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            // // Calculate rects
+            // var charRect = new Rect(position.x, position.y, 30, position.height);
+            // var unitRect = new Rect(position.x + 35, position.y, 50, position.height);
+            // var nameRect = new Rect(position.x + 90, position.y, position.width - 90, position.height);
+            //
+            // // Draw fields - pass GUIContent.none to each so they are drawn without labels
+            // EditorGUI.PropertyField(unitRect, property.FindPropertyRelative("unit"), GUIContent.none);
+            // EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("name"), GUIContent.none);
+            // position.y += 30;
+            var lineSpeakerData = property.boxedValue as LineSpeakerData;
+            // EditorGUI.LabelField(position, lineSpeakerData is LineSpeakerData?"asdas":"zz");
+            var charFieldRect = position;
+            charFieldRect.height = EditorGUIUtility.singleLineHeight;
+            EditorGUI.PropertyField(charFieldRect, property.FindPropertyRelative("Character"), new GUIContent("Character"));
+            if (lineSpeakerData.Character != null)
+            {
+                position.y += EditorGUIUtility.singleLineHeight;
+                var expressions = lineSpeakerData.Character.Expressions
+                    .Where(ced => ced != null)
+                    .Select(ced => ced.ExpressionName).ToArray();
+                // EditorGUI.LabelField(position," :" + lineSpeakerData);
+                if (expressions.Length == 0)
+                {
+                    EditorGUI.HelpBox(position, "NO EXPRESSIONS", MessageType.Error);                    
+                }
+                else
+                {
+                    var expressionProp = property.FindPropertyRelative("Expression");
+
+                    var prevIndex = lineSpeakerData.Character.GetExpressionIndexByName(lineSpeakerData.Expression);
+                    var expressionPopupRect = position;
+                    expressionPopupRect.height = EditorGUIUtility.singleLineHeight;
+                    var expressionIndex = EditorGUI.Popup(expressionPopupRect, prevIndex, expressions);
+                    if (expressionIndex != prevIndex)
+                    {
+                        if (expressionIndex >= 0 && expressionIndex < expressions.Length)
+                        {
+                            expressionProp.objectReferenceValue = lineSpeakerData.Character.Expressions[expressionIndex];
+                            expressionProp.serializedObject.ApplyModifiedProperties();
+                            
+                        }
+                    }
+                    position.y += EditorGUIUtility.singleLineHeight;
+                    var expressionFieldRect = position;
+                    expressionFieldRect.height = EditorGUIUtility.singleLineHeight;
+                    EditorGUI.BeginDisabledGroup(true);
+                    EditorGUI.ObjectField(expressionFieldRect, expressionProp, GUIContent.none);
+                    EditorGUI.EndDisabledGroup();
+                }
+            }
+            else
+            {
+                position.y += EditorGUIUtility.singleLineHeight;
+                EditorGUI.HelpBox(position, "NO CharacterData", MessageType.Error);    
+            }
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Runtime/Core/DialogueSystem.cs
@@ -0,0 +1,163 @@
+
+using System;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.DialogueSystem.Data;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Studio23.SS2.DialogueSystem.Core
+{
+    public class DialogueSystem : MonoBehaviour
+    {
+        public static DialogueSystem Instance;
+
+        [Header("Data")]
+        [SerializeField] private DialogueGraph _currentGraph;
+        [Header("Execution Data")]
+        [SerializeField] private DialogueNodeBase _curNode;
+
+        [Header("Skip")]
+        [SerializeField] private bool _resetSkipAfterChoice = true;
+        [SerializeField] private bool _isSkipActive = false;
+        [SerializeField] private bool _shouldShowLineWhenSkipped = true;
+        [SerializeField] private float _showLineDurationWhenSkipping = .32f;
+        public bool IsSkipActive => _isSkipActive;
+        public bool ShouldShowLineWhenSkipped => _shouldShowLineWhenSkipped;
+        public float ShowLineDurationWhenSkipping => _showLineDurationWhenSkipping;
+        public event Action<bool> OnSkipToggled;
+        public delegate void DialoguePlayEvent(DialogueGraph graph, DialogueNodeBase startNode); 
+        public event DialoguePlayEvent OnDialogueStarted; 
+        public event DialoguePlayEvent OnDialogueEnded; 
+        
+        public event Action<DialogueChoicesNode> OnDialogueChoiceStarted; 
+        public event Action<DialogueChoicesNode> OnDialogueChoiceEnded; 
+        public delegate void DialogueLineEvent(DialogueLineNodeBase dialogueLineNodeBase);
+        
+        /// <summary>
+        /// Subscribe to this to know when dialogue started
+        /// </summary>
+        public DialogueLineEvent DialogueLineStarted;
+
+        /// <summary>
+        /// Subscribe to this to know when dialogue completed
+        /// </summary>
+        public DialogueLineEvent DialogueLineCompleted;
+
+        void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        /// <summary>
+        /// Initialize A new Dialogue Graph
+        /// </summary>
+        /// <param name="newGraph"></param>
+        public void InitializeDialogueGraph(DialogueGraph newGraph)
+        {
+            _currentGraph = newGraph;
+        }
+
+        /// <summary>
+        /// Start playing dialogue and it Fires the Start event
+        /// </summary>
+        public void StartDialogue(DialogueGraph graph)
+        {
+            PlayDialogue(graph).Forget();
+        }
+        
+        public void StartDialogue(DialogueNodeBase node)
+        {
+            PlayDialogue(node).Forget();
+        }
+        
+        public void StartDialogue()
+        {
+            PlayDialogue(_currentGraph).Forget();
+        }
+
+        public async UniTask PlayDialogue(DialogueGraph graph)
+        {
+            graph.Initialize();
+            await PlayDialogue(graph, graph.StartNode);
+        }
+        
+        public async UniTask PlayDialogue(DialogueNodeBase startNode)
+        {
+            var graph = startNode.graph as DialogueGraph;
+            graph.Initialize();
+            await PlayDialogue(graph, startNode);
+        }
+        
+        public async UniTask PlayDialogue(DialogueGraph graph, DialogueNodeBase startNode)
+        {
+            _currentGraph = graph;
+            _curNode = startNode;
+            _currentGraph.HandleDialogueStarted();
+            _isSkipActive = false;
+            
+            OnDialogueStarted?.Invoke(_currentGraph, startNode);
+            while (_curNode != null)
+            {
+                Debug.Log("Play = " + _curNode, _curNode);
+                await _curNode.Play();
+                
+                _curNode = _curNode.GetNextNode();
+            }
+
+            _currentGraph.HandleDialogueEnded();
+            OnDialogueEnded?.Invoke(_currentGraph, startNode);
+        }
+
+        public void AdvanceDialogue()
+        {
+            if (_curNode != null)
+            {
+                _curNode.HandleDialogueAdvance();
+            }
+        }
+
+        public void PickChoice(int choiceIndex)
+        {
+            if (_curNode != null)
+            {
+                if (_resetSkipAfterChoice)
+                {
+                    ToggleSkip(false);
+                }
+                _curNode.HandleChoiceSelected(choiceIndex);
+            }
+        }
+        
+        public void HandleDialogueChoiceStarted(DialogueChoicesNode dialogueChoicesNode)
+        {
+            OnDialogueChoiceStarted?.Invoke(dialogueChoicesNode);
+        }
+
+        public void HandleDialogueChoiceEnded(DialogueChoicesNode dialogueChoicesNode)
+        {
+            OnDialogueChoiceEnded?.Invoke(dialogueChoicesNode);
+        }
+
+        public void ToggleSkip() => ToggleSkip(!_isSkipActive);
+        public void ToggleSkip(bool shouldSkipBeActive) {
+            _isSkipActive = shouldSkipBeActive;
+            OnSkipToggled?.Invoke(_isSkipActive);
+        }
+
+        private void OnDestroy()
+        {
+            if (_currentGraph != null)
+            {
+                _currentGraph.Cleanup();
+            }
+        }
+    }
+}
+
