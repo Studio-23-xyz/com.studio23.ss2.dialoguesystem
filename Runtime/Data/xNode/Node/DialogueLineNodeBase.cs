64b130ae70efda5c5601ff91d81ac36bc541d79b
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2025-01-27 17:26:10 +0600

    Dialogue Start and Complete Boolean Added

---
+++ Runtime/Data/xNode/Node/DialogueLineNodeBase.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.DialogueSystem.Utility;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Tables;
+using UnityEngine.Serialization;
+using XNode;
+
+namespace Studio23.SS2.DialogueSystem.Data
+{
+    public abstract class DialogueLineNodeBase : DialogueNodeBase
+    {
+        public LocalizedString DialogueLocalizedString;
+        [Header("Character Data")] 
+        public LineSpeakerData SpeakerData;
+
+        private bool _hasStarted = false;
+        private bool _hasCompleted = false;
+
+        public bool HasStarted=> _hasStarted;
+        public bool HasCompleted => _hasCompleted;
+        
+        private bool _canAdvanceDialogue;
+        
+        [Node.Output(typeConstraint = TypeConstraint.Strict, connectionType = ConnectionType.Override)]
+        public DialogueLineNodeBase Exit;
+        
+        [Output(typeConstraint =  TypeConstraint.InheritedInverse)] 
+        public EventNodeBase Events;
+
+        public override void HandleDialogueAdvance()
+        {
+            _canAdvanceDialogue = true;
+        }
+        
+        public virtual void InvokePostPlayEvents()
+        {
+            var eventNodePort= GetOutputPort("Events");
+            for (int i = 0; i < eventNodePort.ConnectionCount; i++)
+            {
+                var eventNodeConnection = eventNodePort.GetConnection(i);
+                if (eventNodeConnection != null)
+                {
+                    if (eventNodeConnection.node is EventNodeBase eventNode)
+                    {
+                        eventNode.Invoke();
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"{this} Events port no {i} is connected to {eventNodeConnection.node} but it's not an event node", this);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"{this} Events port no {i} is connected to {eventNodeConnection} but connection null", this);
+                }
+            }
+        }
+
+
+        public override void HandleChoiceSelected(int choiceIndex)
+        {
+            //do nothing
+        }
+
+        public override async UniTask Play()
+        {
+            _canAdvanceDialogue = false;
+            HandleDialogueCompleted(true);
+            if (!Core.DialogueSystem.Instance.IsSkipActive || 
+                Core.DialogueSystem.Instance.ShouldShowLineWhenSkipped)
+            {
+                Core.DialogueSystem.Instance.DialogueLineStarted?.Invoke(this);
+                while (!_canAdvanceDialogue)
+                {
+                    //at this point, we are showing dialogue with ShouldShowLineWhenSkipped = true
+                    if (Core.DialogueSystem.Instance.IsSkipActive)
+                    {
+                        await UniTask.Delay(TimeSpan.FromSeconds(Core.DialogueSystem.Instance.ShowLineDurationWhenSkipping));
+                        break;
+                    }
+
+                    await UniTask.Yield();
+                    await UniTask.NextFrame();
+                }
+                Core.DialogueSystem.Instance.DialogueLineCompleted?.Invoke(this);
+            }
+            
+            InvokePostPlayEvents();
+        }
+
+        public override DialogueNodeBase GetNextNode()
+        {
+            HandleDialogueStarted(true);
+            NodePort outputPort = GetExitPort();
+            if (outputPort == null || outputPort.Connection == null)
+            {
+                return null;
+            }
+            return outputPort.Connection.node as DialogueNodeBase;
+        }
+        
+        public override object GetValue(NodePort port)
+        {
+            if (port.fieldName == "Exit")
+            {
+                return this;
+            }else if (port.fieldName == "Events")
+            {
+                return null;
+            }
+            return base.GetValue(port);
+        }
+
+        public void SetLocalizationTable(TableReference table)
+        {
+            DialogueLocalizedString = new LocalizedString();
+            DialogueLocalizedString.TableReference = table;
+        }
+        
+        public TableReference GetLocalizationTable()
+        {
+            if (DialogueLocalizedString == null)
+            {
+                return default;
+            }
+            return DialogueLocalizedString.TableReference;
+        }
+
+        public string GetLocalizedLineTextInstant() => DialogueLocalizedString.GetLocalizedString();
+        public override void Initialize()
+        {
+            HandleDialogueCompleted(false);
+            HandleDialogueStarted(false);
+        }
+
+        public void HandleDialogueCompleted(bool hasCompleted)
+        {
+            _hasCompleted = hasCompleted;
+        }
+
+        public void HandleDialogueStarted(bool hasStarted)
+        {
+            _hasStarted = hasStarted;
+        }
+    }
+
+}
+
---
+++ Runtime/Data/xNode/Node/DialogueNodeBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.DialogueSystem.Utility;
+using UnityEngine;
+using XNode;
+
+namespace Studio23.SS2.DialogueSystem.Data
+{
+    [NodeWidth(333)]
+    public abstract class DialogueNodeBase : DialogueGraphNodeBase
+    {
+        public NodePort GetExitPort()
+        {
+            return GetOutputPort("Exit");
+        }
+
+        public NodePort GetEntryPort()
+        {
+            return GetInputPort("Entry");
+        }
+
+        public abstract void HandleDialogueAdvance();
+        public abstract void HandleChoiceSelected(int choiceIndex);
+
+
+        public abstract UniTask Play();
+
+        public abstract DialogueNodeBase GetNextNode();
+    }
+
+    public abstract class DialogueBranchingNode : DialogueNodeBase
+    {
+        /// <summary>
+        /// Get all connected nodes.
+        /// DialogueBranchingNodes can have multiple nodes connected to them
+        /// This returns all of them
+        /// Note: returns without checking conditions. Simply returns all connected nodes 
+        /// Note: May alloc GC
+        /// </summary>
+        /// <returns></returns>
+        public abstract IEnumerable<DialogueNodeBase> GetConnectedNodes();
+    }
+}
