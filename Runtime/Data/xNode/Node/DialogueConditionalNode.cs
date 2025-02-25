f0a9a60968c8c1dd6f63d9188a1bb832885aa219
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2025-02-25 15:47:48 +0600

    Dialogue cancel wait fixed

---
+++ Runtime/Data/xNode/Node/DialogueConditionalNode.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Studio23.SS2.DialogueSystem.Runtime.Data;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+namespace Studio23.SS2.DialogueSystem.Data
+{
+    [CreateNodeMenu("Dialogue Conditional node"), NodeTint("#0055AA")]
+    public class DialogueConditionalNode:DialogueConditionalNodeBase
+    {
+        [SerializeReference, SerializeReferenceButton]
+        private List<IDialogueNodeCondition> _conditions = new List<IDialogueNodeCondition>();
+
+        public override void HandleDialogueCancel()
+        {
+            
+        }
+
+        protected override bool CheckConditionsInternal()
+        {
+            foreach (var condition in _conditions)
+            {
+                if (!condition.Evaluate())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        
+    }
+}
